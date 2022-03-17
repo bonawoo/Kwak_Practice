c8c174eef80eff7767a467e1adbbf119e69a9cee
bonawoo <59213213+bonawoo@users.noreply.github.com>
2022-03-17 17:46:53 +0900

    codeup(1081~1084)

---
+++ codeup_100/1082_24164643(AC).cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(){
+    int input;
+    hex(cin);
+    cin >> input;
+    for(int i=1; i<16; i++){
+        cout << hex << uppercase << input << "*" << i << "=" << input*i << endl;
+    }
+    return 0;
+}
---
+++ codeup_100/1083_24164717(AC).cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(){
+    int input;
+    cin >> input;
+    for(int i=1;i<=input;i++){
+        if(i % 3 != 0){
+            cout << i << " ";
+        }else{
+            cout << "X ";
+        }
+    }
+    return 0;
+}
