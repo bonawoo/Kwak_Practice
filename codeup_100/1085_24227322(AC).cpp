066a67b560b0164681397238aa3a2e4316523d09
bonawoo <59213213+bonawoo@users.noreply.github.com>
2022-03-20 21:49:33 +0900

    codeup(1085)

---
+++ codeup_100/1085_24227322(AC).cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(){
+    int h,b,c,s;
+    cin >> h >> b >> c >> s;
+    double total;
+    total = (float)h*b*c*s / (8*1024*1024);
+    cout << fixed;
+    cout.precision(1);
+    cout << total << " MB";
+    return 0;
+}
