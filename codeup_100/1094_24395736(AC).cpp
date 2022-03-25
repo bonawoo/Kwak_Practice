cccf67d35fadebacf45863d0b54378f22bf39e7e
bonawoo <59213213+bonawoo@users.noreply.github.com>
2022-03-25 20:49:03 +0900

    codeup(1094)

---
+++ codeup_100/1094_24395736(AC).cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(){
+    int input;
+    cin >> input;
+    int arr[input];
+    for(int j=0;j<input;j++){
+        cin >> arr[j];
+    }
+    for(int i = input-1; i>=0; i--){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
